e135ae96d494528bcc727c24819fad5eac956a5b
Kevin Mark <kmark1625@gmail.com>
2015-10-25 17:58:02 -0500

    Pre-tail version

--- games/lonely_island/js/game.js
+++ games/lonely_island/js/game.js
@@ -22,6 +22,17 @@ $(".titleButton").click(function () {
   $("#titleScreen").show();
 });
 
+var gameOver = false;
+
+// Game over
+function gameOver() {
+  console.log("Game is over!");
+  gameOver = true;
+}
+
+// Keep track of score
+var points = 0;
+
 // Create the canvas and context
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
@@ -37,13 +48,22 @@ bgImage.onload = function() {
 };
 bgImage.src = "images/background.png";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function() {
-  heroReady = true;
+//Dino image
+var dinoReady = false;
+var dinoImage = new Image();
+dinoImage.onload = function() {
+  dinoReady = true;
 };
-heroImage.src = "images/main_character.png";
+dinoImage.src = "images/dino_spritesheet.png";
+
+
+// NPC image
+var npcReady = false;
+var npcImage = new Image();
+npcImage.onload = function() {
+  npcReady = true;
+};
+npcImage.src = "images/egg.png";
 
 // Monster Image
 var monsterReady = false;
@@ -51,43 +71,120 @@ var monsterImage = new Image();
 monsterImage.onload = function() {
   monsterReady = true;
 };
-monsterImage.src = "images/monster.png";
+monsterImage.src = "images/tree.png";
+
+var initTail = new TailLink(73, 46, "down");
+
 
 // Game objects
-var hero = {
+var dino = {
   speed: 256,
   x: 50,
   y: 50,
+  direction: "down", // up, down, left, right
   imageX: 0,
   imageY: 0,
+  tail: initTail,
   setImageLocation: function (direction) {
     if (direction === "up") {
-      this.imageX = 0;
-      this.imageY = 96;
+      this.imageX = 64;
+      this.imageY = 0;
     } else if (direction === "right") {
-      this.imageX = 0;
-      this.imageY = 64;
+      this.imageX = 32;
+      this.imageY = 0;
     } else if (direction === "down") {
       this.imageX = 0;
       this.imageY = 0;
     } else if (direction === "left") {
-      this.imageX = 0;
-      this.imageY= 32;
+      this.imageX = 138;
+      this.imageY= 0;
     }
   }
 };
 
+// Linked List of tail links
+function TailLink(x, y, direction) {
+  this.x = x;
+  this.y = y;
+  this.direction = direction;
+  this.next;
+}
+
+TailLink.prototype.drawTailLink = function() {
+  context.drawImage(npcImage, 0, 0, 32, 32, this.x, this.y, 20, 20);
+  if (this.next) {
+    this.next.drawTailLink();
+  }
+};
+
+TailLink.prototype.updateLocation = function(x, y, direction) {
+  this.x = x;
+  this.y = y;
+  this.direction=direction;
+  if (this.next) {
+    if (direction === "down"){
+      this.next.updateLocation(this.x, this.y-20, direction);
+    } else if (direction === "up") {
+      this.next.updateLocation(this.x, this.y+20, direction);
+    } else if (direction == "left") {
+      this.next.updateLocation(this.x-20, this.y, direction);
+    } else if (direction == "right") {
+      this.next.updateLocation(this.x+20, this.y, direction);
+    }
+  }
+};
+
+TailLink.prototype.addTailLink = function() {
+  if (!this.next) {// if tail.next is undefined
+    if (this.direction === "down"){
+      // add egg down
+      this.next = new TailLink(this.x, this.y-20, this.direction);
+    } else if (this.direction === "up") {
+      this.next = new TailLink(this.x, this.y+20, this.direction);
+    } else if (this.direction == "left") {
+      this.next = new TailLink(this.x-20, this.y, this.direction);
+    } else if (this.direction == "right") {
+      this.next = new TailLink(this.x+20, this.y, this.direction);
+    }
+  } else {
+    this.next.addTailLink();
+  }
+}
+
+// NPC
+function NPC(x,y) {
+  this.x = x;
+  this.y = y;
+  this.imageX = 0;
+  this.imageY = 0;
+  this.imageWidth = 30;
+  this.imageHeight = 30;
+}
+
+NPC.prototype.setImageProperties = function() {
+  switch(characterNumber) {
+    default:
+      this.imageX = 0;
+      this.imageY = 0;
+      this.imageWidth = 30;
+      this.imageHeight = 30;
+  }
+}
+
+NPC.prototype.newLocation = function() {
+  this.x = 32 + (Math.random() * (canvas.width - 64));
+  this.y = 32 + (Math.random() * (canvas.height - 64));
+}
+
+var npc = new NPC(200, 100);
+
+// Collection of enemies
 function Monster(x, y) {
   this.x = x;
   this.y = y;
 }
 
-// monsterArray = [new Monster(200, 200), new Monster(100,100)];
-
-var monster = {
-  x: 200,
-  y: 200
-};
+var monsters = [];
 
 // Handle keyboard controls
 var keysDown = {};
@@ -99,31 +196,63 @@ addEventListener("keydown", function (e) {
 
 var update = function (modifier) {
   if (38 in keysDown) { // Player holding up
-    hero.y -= hero.speed * modifier;
-    hero.setImageLocation("up");
+    dino.direction = "up";
+    dino.y -= dino.speed * modifier;
+    dino.setImageLocation("up");
+    dino.tail.updateLocation(dino.x + 23, dino.y + 45, "up");
   }
   if (40 in keysDown) { // Player holding down
-    hero.y += hero.speed * modifier;
-    hero.setImageLocation("down");
+    dino.direction = "down";
+    dino.y += dino.speed * modifier;
+    dino.setImageLocation("down");
+    dino.tail.updateLocation(dino.x + 23, dino.y - 4, "down");
   }
   if (37 in keysDown) { // Player holding left
-    hero.x -= hero.speed * modifier
-    hero.setImageLocation("left");
+    dino.direction = "left";
+    dino.x -= dino.speed * modifier;
+    dino.setImageLocation("left");
+    dino.tail.updateLocation(dino.x + 40, dino.y + 20, "up");
   }
   if (39 in keysDown) { // Player holding right
-    hero.x += hero.speed * modifier
-    hero.setImageLocation("right");
+    dino.direction = "right";
+    dino.x += dino.speed * modifier;
+    dino.setImageLocation("right");
+    dino.tail.updateLocation(dino.x + 5, dino.y + 20, "up");
   }
 
   // Collision detection
+  // Detecting enemy collision
+  for (var i=0; i<monsters.length; i++) {
+    if (
+      dino.x <= (monsters[i].x + 20)
+      && monsters[i].x <= (dino.x + 20)
+      && dino.y <= (monsters[i].y + 20)
+      && monsters[i].y <= (dino.y + 20)
+    ) {
+     gameOver();
+    }
+  }
+  // Detecting wall collision
   if (
-    hero.x <= (monster.x + 32)
-    && monster.x <= (hero.x + 32)
-    && hero.y <= (monster.y + 32)
-    && monster.y <= (hero.y + 32)
-  ) { // Redraw the monster randomly
-    monster.x = 32 + (Math.random() * (canvas.width - 64));
-    monster.y = 32 + (Math.random() * (canvas.height - 64));
+    dino.x <= 0
+    || dino.y <= 0
+    || dino.x >= canvas.width - 32
+    || dino.y >= canvas.height - 32
+  ) {
+    gameOver();
+  }
+  // Detecting NPC collision
+  if (
+    dino.x <= (npc.x + 32)
+      && npc.x <= (dino.x + 32)
+      && dino.y <= (npc.y + 32)
+      && npc.y <= (dino.y + 32)
+  ) {
+    npc.newLocation();
+    monsters.push(new Monster(32 + (Math.random() * (canvas.width - 64)), 32 + (Math.random() * (canvas.height - 64))));
+    dino.tail.addTailLink();
+    points++;
+    console.log(points);
   }
 };
 
@@ -131,29 +260,42 @@ var update = function (modifier) {
 var render = function() {
   if (bgReady) {
     context.drawImage(bgImage, 0, 0);
-  };
+  }
 
-  if (heroReady) {
-    context.drawImage(heroImage, hero.imageX, hero.imageY, 32, 32, hero.x, hero.y, 32, 32);
-  };
+  if (dinoReady) {
+    context.drawImage(dinoImage, dino.imageX, dino.imageY, 32, 32, dino.x, dino.y, 50, 50);
+  }
+
+  if (npcReady) {
+    context.drawImage(npcImage, npc.imageX, npc.imageY, npc.imageWidth, npc.imageHeight, npc.x, npc.y, npc.imageWidth, npc.imageHeight)
+    // Draw tail
+    dino.tail.drawTailLink();
+  }
 
   if (monsterReady){
-    context.drawImage(monsterImage, 0, 0, 32, 32, monster.x, monster.y, 32, 32);
-  };
+    for (var i=0; i < monsters.length; i++) {
+      context.drawImage(monsterImage, 0, 0, 80, 100, monsters[i].x, monsters[i].y, 32, 32);
+    }
+  }
+
+  // Draw score
+  context.fillStyle = "black";
+  context.font = "20px Sans-Serif";
+  context.fillText("Score: " + points, 30, 50);
 }
 
 // The main game loop
 var main = function () {
-  var now = Date.now();
-  var delta = now - then;
+    var now = Date.now();
+    var delta = now - then;
 
-  update(delta / 1000);
-  render();
+    update(delta / 1000);
+    render();
 
-  then = now;
+    then = now;
 
-  // Request to do this again ASAP
-  requestAnimationFrame(main);
+    // Request to do this again ASAP
+    requestAnimationFrame(main);
 };
 
 // Cross-browser support for requestAnimationFrame
